4fa7fab2d8686114ecaada41896d07838835ab33
Kenton Vo <kmvo0001@zonemail.clpccd.edu>
2023-04-10 21:00:39 -0700

    1

--- q2.hpp
+++ q2.hpp
@@ -5,3 +5,21 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getInput(int &a,int &b,int &c){
+    cin>>a>>b>>c;
+}
+
+int findMin(int &a,int &b,int &c){
+    int min=a;
+    if(b<a){
+        min=b;
+    }
+    else if(c<min)
+        min=c;
+    return min;
+}
+
+void printResult(int &a,int &b,int &c,int &min){
+    cout<<a<<" "<<b<<" "<<c<<" Min: "<<min<<endl;
+}
